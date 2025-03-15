2a619b511ab542a83b8a6a210bf9432458231fc0
mio <mio@lazym.io>
2025-03-15 21:43:40 +0800

    Re-export anyhow

--- protobuf-parse/src/pure/mod.rs
+++ protobuf-parse/src/pure/mod.rs
@@ -6,5 +6,8 @@ pub mod parse_and_typecheck;
 pub mod parse_dependencies;
 mod parser;
 
+pub mod anyhow {
+    pub use anyhow::*;
+}
 pub use parse_and_typecheck::parse_and_typecheck_custom;
 pub use parse_dependencies::*;

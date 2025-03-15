6feb4199da7c3de843642447bbbeb6ee5c34aea9
mio <mio@lazym.io>
2025-03-15 21:38:28 +0800

    public modules

--- protobuf-parse/src/pure/mod.rs
+++ protobuf-parse/src/pure/mod.rs
@@ -1,9 +1,9 @@
 //! Pure rust `.proto` file parser.
 
-pub(crate) mod convert;
-pub(crate) mod model;
-pub(crate) mod parse_and_typecheck;
-pub(crate) mod parse_dependencies;
+pub mod convert;
+pub mod model;
+pub mod parse_and_typecheck;
+pub mod parse_dependencies;
 mod parser;
 
 pub use parse_and_typecheck::parse_and_typecheck_custom;
